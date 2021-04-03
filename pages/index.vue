442acfc113b47cef86a6da8dcc108dcbb12b6714
osetedev <osetedev@gmail.com>
2021-04-03 13:28:55 +0200

    Update container style

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="container">
+  <div class="container m-auto flex text-center justify-center items-center min-h-screen">
     <div>
       <img src="~/assets/images/pdf.svg" class="w-40 inline" />
       <h1 class="text-8xl py-4 font-light">pedefe</h1>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
+        <NuxtLink
+          to="/merge"
           class="transition-colors duration-500 ease-in-out inline-block px-4 py-2 rounded-md bg-green-100 text-green-700 hover:bg-green-200"
         >
           Merge
-        </a>
+        </NuxtLink>
         <a
           href="https://nuxtjs.org/"
           target="_blank"
@@ -39,41 +37,3 @@ import Vue from 'vue'
 
 export default Vue.extend({})
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
