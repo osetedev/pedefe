00d69c22cbb470ff4cbfb4ebd8dba6ada1d86599
osetedev <osetedev@gmail.com>
2021-04-03 13:18:51 +0200

    Change main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,35 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">pedefe</h1>
+      <img src="~/assets/images/pdf.svg" class="w-40 inline" />
+      <h1 class="text-8xl py-4 font-light">pedefe</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
           target="_blank"
           rel="noopener noreferrer"
-          class="button--green"
+          class="transition-colors duration-500 ease-in-out inline-block px-4 py-2 rounded-md bg-green-100 text-green-700 hover:bg-green-200"
         >
-          Documentation
+          Merge
         </a>
         <a
-          href="https://github.com/nuxt/nuxt.js"
+          href="https://nuxtjs.org/"
           target="_blank"
           rel="noopener noreferrer"
-          class="button--grey"
+          class="transition-colors duration-500 ease-in-out inline-block px-4 py-2 rounded-md bg-blue-100 text-blue-700 hover:bg-blue-200"
         >
-          GitHub
+          Reorder
+        </a>
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="transition-colors duration-500 ease-in-out inline-block px-4 py-2 rounded-md bg-red-100 text-red-700 hover:bg-red-200"
+        >
+          Compress
         </a>
       </div>
+      <div class="pt-5 text-gray-400">Iconos diseñados por <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div>
     </div>
   </div>
 </template>
